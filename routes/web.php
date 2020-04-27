915940ec757f3e15f33478f1f7641799dbfdc419
SultanKs4 <sultangendut@gmail.com>
2020-04-27 10:14:05 +0700

    feat: add login js 12

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use  Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,7 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::redirect('/', 'mahasiswa', 301);
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Route::get('mahasiswa', 'MahasiswaController@index')->name('mahasiswa');
 
@@ -28,3 +31,9 @@ Route::get('mahasiswa/edit/{id}', 'MahasiswaController@edit');
 Route::post('mahasiswa/update/{id}', 'MahasiswaController@update');
 
 Route::get('mahasiswa/hapus/{id}', 'MahasiswaController@hapus');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::resource('/admin/users', 'Admin\UsersController', ['except' => ['show', 'create', 'store']]);
